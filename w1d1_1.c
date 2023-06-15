0a1c7eaa5d890ddd83eed96ac4842fddeb09476c
Nestin91 <93175547+Nestin91@users.noreply.github.com>
2023-06-15 20:28:46 +0530

    Add files via upload

---
+++ w1d1_1.c
@@ -0,0 +1,20 @@
+/*
+ * W1_1.c
+ *
+ *  Created on: 13-Jun-2023
+ *      Author: nesti
+ */
+#include<stdio.h>
+int main()
+{
+	int a=3,b;
+
+	printf("\nEnter a value for b:");
+	fflush(stdout);
+	scanf("%d",&b);
+	printf("\na =%d",a);
+	printf("\nb =%d",b);
+	printf("\nHello World");
+	return 0;
+}
+
